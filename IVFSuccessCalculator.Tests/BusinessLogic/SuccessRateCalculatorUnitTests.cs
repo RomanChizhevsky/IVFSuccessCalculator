8b2409bb0c1f66ed6129306c91b6591ff7e35732
Roman Chizhevsky <RomanChizhevsky@yahoo.com>
2024-12-22 19:18:38 -0500

    Added unit tests around success rate calculator & request payload validator.

---
+++ IVFSuccessCalculator.Tests/BusinessLogic/SuccessRateCalculatorUnitTests.cs
@@ -0,0 +1,303 @@
+﻿using FluentAssertions;
+using IVFSuccessCalculator.BusinessLogic;
+using IVFSuccessCalculator.Domain;
+using Microsoft.Extensions.Options;
+
+namespace IVFSuccessCalculator.Tests.BusinessLogic
+{
+    public class SuccessRateCalculatorUnitTests
+    {
+        private readonly SuccessRateCalculator _calculator;
+        private readonly List<SuccessRateFormulaParameters> _formulaParameters = new();
+
+        private const int DEFAULT_AGE = 20;
+        private const int DEFAULT_HEIGHT = 68; // 5ft8
+        private const int DEFAULT_WEIGHT = 150; // lbs
+
+        public SuccessRateCalculatorUnitTests()
+        {
+            _calculator = new SuccessRateCalculator(Options.Create(_formulaParameters));
+        }
+
+        [Theory]
+        [InlineData(true, true, true, 1)]
+        [InlineData(true, true, false, 2)]
+        [InlineData(true, false, true, 3)]
+        [InlineData(true, false, false, 4)]
+        [InlineData(false, true, true, 5)]
+        [InlineData(false, false, true, 5)]
+        [InlineData(false, true, false, 6)]
+        [InlineData(false, false, false, 6)]
+        public void Calculator_Selects_Appropriate_FormulaParams_BasedOn_SelectionCriteria(bool usingOwnEggs, bool usedIvfBefore, bool reasonForInfertilityKnown, int shouldSelectFormula)
+        {
+            // ARRANGE
+            bool? dontCare_UsedIvf = null;
+
+            _formulaParameters.AddRange([
+                new SuccessRateFormulaParameters(usingOwnEggs: true, usedIvfBefore: true, reasonForInfertilityKnown: true) {
+                    Intercept = -6.0
+                },
+
+                new SuccessRateFormulaParameters(usingOwnEggs: true, usedIvfBefore: true, reasonForInfertilityKnown: false) {
+                    Intercept = -6.1
+                },
+
+                new SuccessRateFormulaParameters(usingOwnEggs: true, usedIvfBefore: false, reasonForInfertilityKnown: true) {
+                    Intercept = -6.2
+                },
+
+                new SuccessRateFormulaParameters(usingOwnEggs: true, usedIvfBefore: false, reasonForInfertilityKnown: false) {
+                    Intercept = -6.3
+                },
+
+                new SuccessRateFormulaParameters(usingOwnEggs: false, dontCare_UsedIvf, reasonForInfertilityKnown: true) {
+                    Intercept = -6.4
+                },
+
+                new SuccessRateFormulaParameters(usingOwnEggs: false, dontCare_UsedIvf, reasonForInfertilityKnown: false) {
+                    Intercept = -6.5
+                },
+             ]);
+
+            var request = new SuccessRateCalculationRequest
+            {
+                Age = DEFAULT_AGE,
+                Height = DEFAULT_HEIGHT,
+                Weight = DEFAULT_WEIGHT,
+
+                InfertilityDiagnosis = new(),
+
+                UsingOwnEggs = usingOwnEggs,
+                UsedIvfBefore = usedIvfBefore,
+                ReasonForInfertilityKnown = reasonForInfertilityKnown,
+            };
+
+            // ACT
+            var result = _calculator.Calculate(request);
+
+            // ASSERT
+            var expectedScore = _formulaParameters[shouldSelectFormula - 1].Intercept;
+            result.Should().Be(Sigmoid(expectedScore));
+        }
+
+        [Fact]
+        public void Calculates_BasedOn_Age()
+        {
+            // ARRANGE
+            var age = 20;
+
+            var linearCoeff = 0.3;
+            var powerCoeff = -0.0003;
+            var powerFactor = 2.7;
+
+            _formulaParameters.Add(new SuccessRateFormulaParameters(default, default, default)
+            {
+                AgeLinearCoefficient = linearCoeff,
+                AgePowerCoefficient = powerCoeff,
+                AgePowerFactor = powerFactor
+            });
+
+            var request = new SuccessRateCalculationRequest
+            {
+                Age = age,
+
+                Height = DEFAULT_HEIGHT,
+                Weight = DEFAULT_WEIGHT,
+                InfertilityDiagnosis = new()
+            };
+
+            // ACT
+            var result = _calculator.Calculate(request);
+
+            // ASSERT
+            var expectedAgeScore = (linearCoeff * age) + (powerCoeff * Math.Pow(age, powerFactor));
+            result.Should().Be(Sigmoid(expectedAgeScore));
+        }
+
+        [Fact]
+        public void Calculates_BasedOn_Bmi()
+        {
+            // ARRANGE
+            var weight = 150;
+
+            const int FT = 12;
+            var height = 5 * FT + 8;
+
+            var linearCoeff = 0.05;
+            var powerCoeff = -0.0009;
+            var powerFactor = 2.0;
+
+            _formulaParameters.Add(new SuccessRateFormulaParameters(default, default, default)
+            {
+                BmiLinearCoefficient = linearCoeff,
+                BmiPowerCoefficient = powerCoeff,
+                BmiPowerFactor = powerFactor
+            });
+
+            var request = new SuccessRateCalculationRequest
+            {
+                Age = DEFAULT_AGE,
+                Height = height,
+                Weight = weight,
+
+                InfertilityDiagnosis = new()
+            };
+
+            // ACT
+            var result = _calculator.Calculate(request);
+
+            // ASSERT
+            var bmi = 22.8;
+            var expectedBmiScore = (linearCoeff * bmi) + (powerCoeff * Math.Pow(bmi, powerFactor));
+
+            result.Should().BeApproximately(Sigmoid(expectedBmiScore), 1e-5);
+        }
+
+        
+
+        [Fact]
+        public void Calculates_Based_On_Known_IvfFactors()
+        {
+            // ARRANGE
+            _formulaParameters.Add(new SuccessRateFormulaParameters(default, default, true)
+            {
+                TubalFactor = 0.1,
+                MaleInfertilityFactor = 0.2,
+                EndometriosisFactor = 0.02,
+                OvulatoryDisorderFactor = 0.25,
+                DiminishedOvarianReserveFactor = -0.5,
+                UterineFactor = -0.1,
+                SupplementalFactor = -0.05,
+
+                // This should not be factored into the calculation
+                UnexplainedInfertilityFactor = 0.2
+            });
+
+            var request = new SuccessRateCalculationRequest
+            {
+                Age = DEFAULT_AGE,
+                Height = DEFAULT_HEIGHT,
+                Weight = DEFAULT_WEIGHT,
+
+                ReasonForInfertilityKnown = true,
+                InfertilityDiagnosis = new InfertilityFactors
+                {
+                    TubalFactor = true,
+                    MaleFactorInf = true,
+                    Endometriosis = true,
+                    OvulatoryDisorder = true,
+                    DiminishedOvarianReserve = true,
+                    UterineFactor = true,
+                    OtherReason = true
+                }
+            };
+
+            // ACT
+            var result = _calculator.Calculate(request);
+
+            // ASSERT
+            var ivf_scores = 0.1 + 0.2 + 0.02 + 0.25 + -0.5 + -0.1 + -0.05;
+            result.Should().Be(Sigmoid(ivf_scores));
+        }
+
+        [Fact]
+        public void Calculates_Based_On_Unexplained_Infertility()
+        {
+            // ARRANGE
+            _formulaParameters.Add(new SuccessRateFormulaParameters(default, default, true)
+            {
+                UnexplainedInfertilityFactor = 0.2,
+
+                // These should not be factored into the calculation
+                TubalFactor = 0.1,
+                MaleInfertilityFactor = 0.2,
+                EndometriosisFactor = 0.02,
+                OvulatoryDisorderFactor = 0.25,
+                DiminishedOvarianReserveFactor = -0.5,
+                UterineFactor = -0.1,
+                SupplementalFactor = -0.05,
+            });
+
+            var request = new SuccessRateCalculationRequest
+            {
+                Age = DEFAULT_AGE,
+                Height = DEFAULT_HEIGHT,
+                Weight = DEFAULT_WEIGHT,
+
+                ReasonForInfertilityKnown = true,
+                InfertilityDiagnosis = new InfertilityFactors
+                {
+                    UnexplainedInf = true
+                }
+            };
+
+            // ACT
+            var result = _calculator.Calculate(request);
+
+            // ASSERT
+            var ivf_scores = 0.2;
+            result.Should().Be(Sigmoid(ivf_scores));
+        }
+
+        [Fact]
+        public void Calculates_Based_On_Unknown_Infertility()
+        {
+            // ARRANGE
+            _formulaParameters.Add(new SuccessRateFormulaParameters(default, default, false)
+            {
+                // None of these should not be factored into the calculation
+                TubalFactor = 0.1,
+                MaleInfertilityFactor = 0.2,
+                EndometriosisFactor = 0.02,
+                OvulatoryDisorderFactor = 0.25,
+                DiminishedOvarianReserveFactor = -0.5,
+                UterineFactor = -0.1,
+                SupplementalFactor = -0.05,
+            });
+
+            var request = new SuccessRateCalculationRequest
+            {
+                Age = DEFAULT_AGE,
+                Height = DEFAULT_HEIGHT,
+                Weight = DEFAULT_WEIGHT,
+
+                ReasonForInfertilityKnown = false,
+                InfertilityDiagnosis = new InfertilityFactors()
+            };
+
+            // ACT
+            var result = _calculator.Calculate(request);
+
+            // ASSERT
+            result.Should().Be(Sigmoid(0));
+        }
+
+        [Fact]
+        public void Calculation_Incorporates_Formula_Intercept()
+        {
+            // ARRANGE
+            _formulaParameters.Add(new SuccessRateFormulaParameters(default, default, false)
+            {
+                Intercept = -7.0
+            });
+
+            var request = new SuccessRateCalculationRequest
+            {
+                Age = DEFAULT_AGE,
+                Height = DEFAULT_HEIGHT,
+                Weight = DEFAULT_WEIGHT,
+
+                ReasonForInfertilityKnown = false,
+                InfertilityDiagnosis = new InfertilityFactors()
+            };
+
+            // ACT
+            var result = _calculator.Calculate(request);
+
+            // ASSERT
+            result.Should().Be(Sigmoid(-7.0));
+        }
+
+        public double Sigmoid(double value) => Math.Exp(value) / (1 + Math.Exp(value));
+    }
+}
---
+++ IVFSuccessCalculator.Tests/BusinessLogic/SuccessRateRequestValidatorTests.cs
@@ -0,0 +1,252 @@
+﻿using FluentAssertions;
+using IVFSuccessCalculator.BusinessLogic;
+using IVFSuccessCalculator.Domain;
+
+namespace IVFSuccessCalculator.Tests.BusinessLogic
+{
+    public class SuccessRateRequestValidatorTests
+    {
+        private readonly SuccessRateRequestValidator _validator;
+        private readonly SuccessRateCalculationRequest _request;
+
+        private const int FT = 12;
+
+        public SuccessRateRequestValidatorTests()
+        {
+            _validator = new SuccessRateRequestValidator();
+            _request = new SuccessRateCalculationRequest
+            {
+                Age = 30,
+                Height = 5*FT + 8,
+                Weight = 150,
+
+                ReasonForInfertilityKnown = false,
+                InfertilityDiagnosis = new InfertilityFactors(),
+
+                NumLiveBirths = 0,
+                NumPriorPregnancies = 0,
+            };
+        }
+
+        [Theory]
+        [InlineData(19)]
+        [InlineData(51)]
+        public void Validation_Fails_When_Age_Is_Outside_Allowable_Range(int invalidAge)
+        {
+            _request.Age = invalidAge;
+
+            var response = _validator.Validate(_request);
+
+            response.IsValid.Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData(20)]
+        [InlineData(30)]
+        [InlineData(50)]
+        public void Validation_Succeeds_When_Age_Is_Within_Allowable_Range(int validAge)
+        {
+            _request.Age = validAge;
+
+            var response = _validator.Validate(_request);
+
+            response.IsValid.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(4, 5)]
+        [InlineData(6, 1)]
+        public void Validation_Fails_When_Height_Is_Outside_Allowable_Range(int invalidFt, int invalidInches)
+        {
+            _request.Height = invalidFt * FT + invalidInches;
+
+            var response = _validator.Validate(_request);
+
+            response.IsValid.Should().BeFalse();
+
+            response.Errors.Count().Should().Be(1);
+            response.Errors[0].ErrorMessage.Should().Be($"'Height' must be between 4'6\" and 6'. You entered {invalidFt}'{invalidInches}\"");
+        }
+
+        [Theory]
+        [InlineData(4 * FT + 6)]
+        [InlineData(5*FT + 2)]
+        [InlineData(6 * FT)]
+        public void Validation_Succeeds_When_Height_Is_Within_Allowable_Range(int validHeight)
+        {
+            _request.Height = validHeight;
+
+            var response = _validator.Validate(_request);
+
+            response.IsValid.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(79)]
+        [InlineData(301)]
+        public void Validation_Fails_When_Weight_Is_Outside_Allowable_Range(int invalidWeight)
+        {
+            _request.Weight = invalidWeight;
+
+            var response = _validator.Validate(_request);
+
+            response.IsValid.Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData(80)]
+        [InlineData(150)]
+        [InlineData(300)]
+        public void Validation_Succeeds_When_Weight_Is_Within_Allowable_Range(int validWeight)
+        {
+            _request.Weight = validWeight;
+
+            var response = _validator.Validate(_request);
+
+            response.IsValid.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Validation_Fails_When_Num_Prior_Pregnancies_LessThanZero()
+        {
+            _request.NumPriorPregnancies = -1;
+
+            var response = _validator.Validate(_request);
+
+            response.IsValid.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Validation_Fails_When_Num_Live_Births_LessThanZero()
+        {
+            _request.NumLiveBirths = -1;
+
+            var response = _validator.Validate(_request);
+
+            response.IsValid.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Validation_Fails_When_Num_Live_Births_Exceeds_Num_PriorPregnancies()
+        {
+            _request.NumPriorPregnancies = 2;
+            _request.NumLiveBirths = 3;
+
+            var response = _validator.Validate(_request);
+
+            response.IsValid.Should().BeFalse();
+
+            response.Errors.Count().Should().Be(1);
+            response.Errors[0].ErrorMessage.Should().Be("The number of live births cannot exceed the number of prior pregnancies.");
+        }
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(2, 1)]
+        public void Validation_Succeeds_When_Num_Live_Births_And_PriorPregnancies_InRange(int priorPregnancies, int numLiveBirths)
+        {
+            _request.NumPriorPregnancies = priorPregnancies;
+            _request.NumLiveBirths = numLiveBirths;
+
+            var response = _validator.Validate(_request);
+
+            response.IsValid.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Validation_Fails_When_InfFactors_Specified_And_Reason_Unknown()
+        {
+            _request.ReasonForInfertilityKnown = false;
+            _request.InfertilityDiagnosis = new InfertilityFactors
+            {
+                UterineFactor = true,
+                OvulatoryDisorder = true
+            };
+
+            var response = _validator.Validate(_request);
+
+            response.IsValid.Should().BeFalse();
+
+            response.Errors.Count().Should().Be(1);
+            response.Errors[0].ErrorMessage.Should().Be("Infertility factors cannot be specified when reason for infertility is not known.");
+        }
+
+        [Fact]
+        public void Validation_Fails_When_No_InfFactors_Specified_And_Reason_Known()
+        {
+            _request.ReasonForInfertilityKnown = true;
+            _request.InfertilityDiagnosis = InfertilityFactors.None;
+
+            var response = _validator.Validate(_request);
+
+            response.IsValid.Should().BeFalse();
+
+            response.Errors.Count().Should().Be(1);
+            response.Errors[0].ErrorMessage.Should().Be("At least one infertility factor must be specified.");
+        }
+
+        [Fact]
+        public void Validation_Fails_When_Unexplained_And_Other_InfFactors_Specified()
+        {
+            _request.ReasonForInfertilityKnown = true;
+            _request.InfertilityDiagnosis = new InfertilityFactors
+            {
+                UnexplainedInf = true,
+                Endometriosis = true
+            };
+
+            var response = _validator.Validate(_request);
+
+            response.IsValid.Should().BeFalse();
+
+            response.Errors.Count().Should().Be(1);
+            response.Errors[0].ErrorMessage.Should().Be("Infertility factors cannot be specified if infertility is unexplained.");
+        }
+
+        [Theory]
+        [MemberData(nameof(ExampleValidRequests))]
+        public void Validation_Succeeds_When_Request_Specified(SuccessRateCalculationRequest validRequest)
+        {
+            var response = _validator.Validate(validRequest);
+            response.IsValid.Should().BeTrue();
+        }
+
+        public static IEnumerable<object[]> ExampleValidRequests => [
+            // Infertility Diagnosis not performed (Reason for infertility is not yet known)
+            [new SuccessRateCalculationRequest {
+                Age = 20,
+                Height = 5*FT + 8,
+                Weight = 150,
+
+                ReasonForInfertilityKnown = false,
+                InfertilityDiagnosis = new()
+            }],
+
+            // Infertility Diagnosis - Infertility cannot be explained after evaluation
+            [new SuccessRateCalculationRequest {
+                Age = 20,
+                Height = 5*FT + 8,
+                Weight = 150,
+
+                ReasonForInfertilityKnown = true,
+                InfertilityDiagnosis = new InfertilityFactors {
+                    UnexplainedInf = true
+                }
+            }],
+
+            // Infertility Diagnosis - Various known factors specified
+            [new SuccessRateCalculationRequest {
+                Age = 20,
+                Height = 5*FT + 8,
+                Weight = 150,
+
+                ReasonForInfertilityKnown = true,
+                InfertilityDiagnosis = new InfertilityFactors {
+                    MaleFactorInf = true,
+                    Endometriosis = true,
+                    DiminishedOvarianReserve = true
+                }
+            }],
+        ];
+    }
+}
